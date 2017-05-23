704456f406f33cb6bf6ebffc1ba6b1043f087034
Arda Bozok <abozok@kavibilisim.com>
2017-05-23 16:59:50 +0300

    23.05.17

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+    recipes: Recipe[] = [
+                         new Recipe('Arda', 'Bu bir açıklamadır', 'http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-7_edited.jpg'),
+                         new Recipe('Arda', 'Bu bir açıklamadır', 'http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-7_edited.jpg'),
+];
 
   constructor() { }
 
